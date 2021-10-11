724197dca6d4c8fd987db6a93a8249b0e51ac3e4
Maria Budzanowska <budzanowska.maria@gmail.com>
2021-10-11 13:15:56 +0200

    rendering a map with google maps based on name of the location provided

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,48 @@
-// Code goes here!
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyBamTJUVarBxUvo8ZAi3eKsKFQbyV8BmV8";
+
+declare var google: any;
+
+type GoogleGocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+function searchAddressHanlder(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  axios
+    .get<GoogleGocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      console.log(coordinates);
+      const map = new google.maps.Map(
+        document.getElementById("map") as HTMLElement,
+        {
+          center: coordinates,
+          zoom: 8,
+        }
+      );
+      new google.maps.Marker({
+        position: coordinates,
+        map: map,
+      });
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
+}
+
+form.addEventListener("submit", searchAddressHanlder);
